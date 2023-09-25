bd35586358dedf2c276e43014d0e60971e935619
Khalil <144478194+khalilhh20@users.noreply.github.com>
2023-09-25 15:41:30 -0400

    Add files via upload

---
+++ Assignment2.R
@@ -0,0 +1,20 @@
+
+	Pearson's product-moment correlation
+
+data:  my_data$wt and my_data$mpg
+t = -9.559, df = 30, p-value = 1.294e-10
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ -0.9338264 -0.7440872
+sample estimates:
+       cor 
+-0.8676594 
+
+Spearman's rank correlation rho
+
+data:  my_data$wt and my_data$mpg
+S = 10292, p-value = 1.488e-11
+alternative hypothesis: true rho is not equal to 0
+sample estimates:
+      rho 
+-0.886422 
