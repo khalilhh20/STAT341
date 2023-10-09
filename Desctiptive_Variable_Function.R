53504f7ef8fb11c7c5a5e92f23f503dd93cb12c9
Khalil <144478194+khalilhh20@users.noreply.github.com>
2023-10-09 15:36:26 -0400

    Add files via upload

---
+++ Desctiptive_Variable_Function.R
@@ -0,0 +1,23 @@
+variable1 <- c(1,23,4,5,6,7,8,9,10,1900)
+# calculate mean for variable1
+mean(variable1)
+
+# calculate median for variable1
+median (variable1)
+
+# calculate standard deviation for variable 1
+sd(variable1)
+
+# calculate mode for variable1
+
+library(DescTools)
+install.packages('DescTools')
+
+Mode (variable1)
+
+# Calculate range or min/max values for variable1
+min(variable1)
+max(variable1)
+
+# calculate variants for variable1
+var (variable1) 
